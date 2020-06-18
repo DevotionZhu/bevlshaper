bdabc15fbb5fdda93866e70ef6ec910719dbcc33
SCUD3R1A <1u290bu9012u49b12u409ub12094bu1290b4u9012u4b@1284b8912u429014ub9012u4b2901u4b90124u12.de>
2020-06-18 13:04:46 +0200

    Update.

--- lshapenet/generate_synth_train_data.py
+++ lshapenet/generate_synth_train_data.py
@@ -1,8 +1,9 @@
 # Import dependencies
-import numpy as np
-
 import cv2
 import numpy as np
+
+# Derive constants
+DEG_TO_RAD = np.pi / 180
 
 # Create blank image
 def create_blank(width, height, rgb_color=(255, 255, 255)):
@@ -12,36 +13,41 @@ def create_blank(width, height, rgb_color=(255, 255, 255)):
     return image
 
 # Constants
-MAP_WIDTH = 1000
-SENSOR_POSITION = (round(MAP_WIDTH) / 2, 0)
-NUMBER_OF_VEHICLES_MEAN, NUMBER_OF_VEHICLES_STD = 8.0, 5.0
-VEHICLE_LENGTH_MEAN, VEHICLE_LENGTH_STD = 4.2, 0.8
+MAP_WIDTH = 100
+METER_TO_PIXEL = 10
+
+NUMBER_OF_VEHICLES_MEAN, NUMBER_OF_VEHICLES_STD = 5.0, 0.0
+VEHICLE_LENGTH_MEAN, VEHICLE_LENGTH_STD = 4.2, 0.2
 VEHICLE_WIDTH_MEAN, VEHICLE_WIDTH_STD = 1.8, 0.3
-VEHICLE_ANGLE_MEAN, VEHICLE_ANGLE_STD = 90.0, 0.8
+VEHICLE_ANGLE_MEAN, VEHICLE_ANGLE_STD = 90.0 * DEG_TO_RAD, 0.05
+
+# Determine sensor position
+sensor_position = (round(MAP_WIDTH) / 2, 0)
 
 # Init empty image
-img = create_blank(MAP_WIDTH, MAP_WIDTH)
+img = create_blank(MAP_WIDTH * METER_TO_PIXEL, MAP_WIDTH * METER_TO_PIXEL)
 
 # Randomly manipulate number of vehicles
 # TODO ADD NOISE
-number_of_vehicles = int(max(1, np.round(np.random.uniform(NUMBER_OF_VEHICLES_MEAN, NUMBER_OF_VEHICLES_STD, 1)))[0])
+number_of_vehicles = int(max(1, np.round(np.random.uniform(NUMBER_OF_VEHICLES_MEAN, NUMBER_OF_VEHICLES_STD, 1))))
 
 # Create one vehicle per iteration
 for i in range(number_of_vehicles):
     # Set first point and calculate all other vehicle parameters
-    p1 = np.asarray([np.random.uniform(0.0, MAP_WIDTH, 1), np.random.uniform(0.0, MAP_WIDTH, 1)]
-    vehicle_length = np.random.normal(VEHICLE_LENGTH_MEAN, VEHICLE_LENGTH_STD, 1)
-    vehicle_width = np.random.normal(VEHICLE_WIDTH_MEAN, VEHICLE_WIDTH_STD, 1)
+    p1 = np.around(np.random.uniform(0.0, MAP_WIDTH, (2))).astype(np.int32) * METER_TO_PIXEL
+    vehicle_length = np.random.normal(VEHICLE_LENGTH_MEAN, VEHICLE_LENGTH_STD, 1) * METER_TO_PIXEL
+    vehicle_width = np.random.normal(VEHICLE_WIDTH_MEAN, VEHICLE_WIDTH_STD, 1) * METER_TO_PIXEL
     vehicle_angle = np.random.normal(VEHICLE_ANGLE_MEAN, VEHICLE_ANGLE_STD, 1)
 
     # Calculate all other points
-    p2 = [p1 + vehicle_length * np.cos(vehicle_angle), p1 + vehicle_length * np.sin(vehicle_angle)]
-    p3 = [p1 + vehicle_width * np.sin(vehicle_angle), p1 + vehicle_width * np.cos(vehicle_angle)]
-    p4 = [p2 + vehicle_width * np.sin(vehicle_angle), p2 + vehicle_width * np.cos(vehicle_angle)]
+    p2 = p1 + np.around(np.asarray([vehicle_length * np.cos(vehicle_angle), vehicle_length * np.sin(vehicle_angle)])).astype(np.int32).reshape(2)
+    p3 = p1 + np.around(np.asarray([vehicle_width * np.cos(vehicle_angle), vehicle_width * np.sin(vehicle_angle)])).astype(np.int32).reshape(2)
+    p4 = p2 + np.around(np.asarray([vehicle_width * np.cos(vehicle_angle), vehicle_width * np.sin(vehicle_angle)])).astype(np.int32).reshape(2)
 
     # Draw rectangle
-    cv2.rectangle(img, p1, p4, color=(255, 20, 20), thickness=3)
+    cv2.rectangle(img, tuple(p1), tuple(p4), color=(255, 20, 20), thickness=2)
 
 # Show results
+img = cv2.transpose(img)
 cv2.imshow("Results", img)
 cv2.waitKey(0)
